7b7fc2b72bf03a4cc2ccbfaabe995247cea64e6c
Eduardo Robles Elvira <edulix@gmail.com>
2021-08-25 10:27:55 +0200

    embedding the iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { FormattedMessage } from "react-intl"
-import styled from "styled-components"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import "./index.css"
 
-const StyledComingSoon = styled.div`
-  text-align: center;
-  width: 100%;
-  padding-top: 200px;
-`
 
-function LayoutedIndexPage({title, description, showCallToAction}) {
-  if (title === null) {
-    title = ""
-  }
-  if (description === null) {
-    description = ""
-  }
-
+function IndexPage() {
   return (
-    <>
-      <SEO
-        title={'Boda de Agus y Fabi'}
-        keywords={['Boda de Agus y Fabi']}
-      />
-
-      <StyledComingSoon className="column">
-        <FormattedMessage id="index.comingSoon" />
-      </StyledComingSoon>
-    </>
+    <iframe 
+      src="https://docs.google.com/forms/d/e/1FAIpQLSc_dKRIpJxkBNPF4sdvtk1lodD2lpGG7choSD0fBfRDJRhBJQ/viewform?embedded=true"
+      width="100%" 
+      height="2200" 
+      frameborder="0" 
+      marginheight="0" 
+      marginwidth="0"
+    >
+      Cargando…
+    </iframe>
   )
-}
-
-function IndexPage({title, description, showCallToAction}) {
-  // LayoutedIndexPage is another function because Layout declares the 
-  // IntlProvider and only inside the layout can it be used
-  return (
-    <Layout>
-      <LayoutedIndexPage
-        title={title}
-        description={description}
-        showCallToAction={showCallToAction}
-      />
-    </Layout>
-  )
-}
-
-IndexPage.defaultProps = {
-  title: null,
-  description: null,
-  showCallToAction: true
-}
-
-IndexPage.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  showCallToAction: PropTypes.bool
 }
 
 export default IndexPage
