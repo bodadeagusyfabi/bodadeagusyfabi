063590cfbadc0207656632b8a12e8605f301cf66
Eduardo Robles Elvira <edulix@gmail.com>
2021-08-25 11:00:25 +0200

    adding header image and background color, hopefully

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
-import PropTypes from "prop-types"
+import styled from "styled-components"
 
+import HeaderImageSrc from "../images/header.png"
 import "./index.css"
 
+const HeaderImg = styled.div`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 8px;
+`
+
+const IframeStyled = styled.iframe`
+  background-color: #f2ecec;
+`
 
 function IndexPage() {
   return (
-    <iframe 
-      src="https://docs.google.com/forms/d/e/1FAIpQLSc_dKRIpJxkBNPF4sdvtk1lodD2lpGG7choSD0fBfRDJRhBJQ/viewform?embedded=true"
-      width="100%" 
-      height="2200" 
-      frameborder="0" 
-      marginheight="0" 
-      marginwidth="0"
-    >
-      Cargando…
-    </iframe>
+    <>
+      <HeaderImg src={HeaderImageSrc} />
+      <IframeStyled 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSc_dKRIpJxkBNPF4sdvtk1lodD2lpGG7choSD0fBfRDJRhBJQ/viewform?embedded=true"
+        width="100%" 
+        height="2200" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0"
+      >
+        Cargando…
+      </IframeStyled>
+    </>
   )
 }
 
